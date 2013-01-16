3213cbc0557bcccd1cfb0041ebf4a0c8f10fd55d
Elmer Garduno <garduno@gmail.com>
2013-01-16 00:42:44 -0500

    Updated versions

--- src/main/java/edu/cmu/lti/oaqa/bio/framework/eval/passage/CSEPassageMAPEvalPersistenceProvider.java
+++ src/main/java/edu/cmu/lti/oaqa/bio/framework/eval/passage/CSEPassageMAPEvalPersistenceProvider.java
@@ -21,7 +21,7 @@ import edu.cmu.lti.oaqa.framework.persistence.AbstractPassageMAPEvalPersistenceP
 public class CSEPassageMAPEvalPersistenceProvider extends AbstractPassageMAPEvalPersistenceProvider {
 
   @Override
-  public void deletePassageAggrEval(final Key key, final int sequenceId) {
+  public void deletePassageAggrEval(final Key key, final String sequenceId) {
     final String name = getClass().getSimpleName();
     String insert = getDeletePassageAggrEval();
     DataStoreImpl.getInstance().jdbcTemplate().update(insert, new PreparedStatementSetter() {
@@ -29,13 +29,13 @@ public class CSEPassageMAPEvalPersistenceProvider extends AbstractPassageMAPEval
         ps.setString(1, key.getExperiment());
         ps.setString(2, key.getTrace().getTraceHash());
         ps.setString(3, name);
-        ps.setInt(4, sequenceId);
+        ps.setString(4, sequenceId);
       }
     });
   }
 
   @Override
-  public void insertPartialCounts(final Key key, final int sequenceId, final PassageMAPCounts counts)
+  public void insertPartialCounts(final Key key, final String sequenceId, final PassageMAPCounts counts)
           throws SQLException {
     final String eName = getClass().getSimpleName();
     String insert = getInsertPassageAggregates();
@@ -49,7 +49,7 @@ public class CSEPassageMAPEvalPersistenceProvider extends AbstractPassageMAPEval
         ps.setFloat(5, counts.getPsgavep());
         ps.setFloat(6, counts.getAspavep());
         ps.setFloat(7, counts.getCount());
-        ps.setInt(8, sequenceId);
+        ps.setString(8, sequenceId);
         ps.setInt(9, key.getStage());
         ps.setString(10, trace.getTraceHash());
       }
